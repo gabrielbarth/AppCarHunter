7b4dc63a4e626d507c9cf66ca9f32a662eb6ff9d
gabrielbarth <gabriel.silverio@stone.com.br>
2024-11-09 01:58:29 -0400

    add navigation compose

--- app/src/main/java/com/example/carhunter/MainActivity.kt
+++ app/src/main/java/com/example/carhunter/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.carhunter.pages.login.LoginScreen
-import com.example.carhunter.pages.login.LoginViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,29 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                val viewModel: LoginViewModel = viewModel()
-                LoginScreen(
-                    viewModel,
-                    activity = this,
-                    onSignInSuccess = { println("login sucess!") })
+                MyApplication()
             }
+
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        fetchItems()
-    }
-
-    private fun setupView() {
-        // TODO
-    }
-
-    private fun requestLocationPermission() {
-        // TODO
-    }
-
-    private fun fetchItems() {
-        // TODO
-    }
 }
---
+++ app/src/main/java/com/example/carhunter/MyApplication.kt
@@ -0,0 +1,56 @@
+package com.example.carhunter
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.carhunter.ui.carList.CarListScreen
+import com.example.carhunter.ui.login.LoginScreen
+
+private object Screens {
+    const val LOGIN = "login"
+    const val CAR_LIST = "carList"
+    const val CAR_DETAILS = "carDetails"
+    const val CAR_FORM = "carForm"
+}
+
+object Arguments {
+    const val CAR_ID = "carId"
+}
+
+private object Routes {
+    const val LOGIN = Screens.LOGIN
+    const val CAR_LIST = Screens.CAR_LIST
+    const val CAR_DETAILS = "${Screens.CAR_DETAILS}/{${Arguments.CAR_ID}}"
+    const val CAR_FORM =
+        "${Screens.CAR_FORM}?${Arguments.CAR_ID}={${Arguments.CAR_ID}}"
+}
+
+@Composable
+fun MyApplication(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = Screens.LOGIN
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Routes.LOGIN) {
+            LoginScreen(
+                onSignInSuccess = {
+                    println("NavHost -> onSignInSuccess")
+                    navController.navigate(Screens.CAR_LIST)
+                },
+            )
+        }
+        composable(route = Routes.CAR_LIST) {
+            CarListScreen()
+        }
+
+    }
+
+}
